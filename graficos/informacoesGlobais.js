5e7b7aa5c8b0ab997ccb7d50a63e451b9010fd33
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-10-25 11:36:18 +0000

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,5 +7,16 @@ async function visualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
 
-    paragrafo.innerHTML = 'Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados_pessoas_conectadas}'
+    paragrafo.innerHTML = `Você sabia que o mundo /
+    tem <span>${dados.total_pessoas_mundo}</span> /
+     de pessoas e que aproximadamente /
+     <span>${dados.total_pessoas_conectadas}</span> estão /
+     conectadas em alguma rede social e passam /
+     em média <span>${dados.tempo_medio}</span> /
+     horas conectadas.`
+     console.log(paragrafo)
+
+     const container = document.getElementById('graficos-container')
+     container.appendChild(paragrafo)
 }
+visualizarInformacoesGlobais()
