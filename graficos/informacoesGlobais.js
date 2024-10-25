1bdb54402412de075dfc3a3c2b7da1c2f3ab7d9e
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-10-25 11:15:51 +0000

    const

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,3 +1,11 @@
 const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+
+    paragrafo.innerHTML = 'Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados_pessoas_conectadas}'
+}
