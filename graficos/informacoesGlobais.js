a2c7f1afd833a683ba255de5a4d1ef0c5f597ccb
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-11-08 11:09:03 +0000

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,11 +3,13 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
     const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container_texto')
-
-    paragrafo.innerHTML = `Você sabia que o mundo 
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo
     tem <span>${dados.total_pessoas_mundo}</span> 
      de pessoas e que aproximadamente 
      <span>${dados.total_pessoas_conectadas}</span> estão 
